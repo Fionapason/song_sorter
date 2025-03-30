da9c04a5886a647a94f698742f05dddbe7b048c1
Fionapason <96210418+Fionapason@users.noreply.github.com>
2025-03-30 15:05:37 +0200

    Add files via upload
    first commit

---
+++ song_sorter/src/song_lists.js
@@ -0,0 +1,69 @@
+window.john_list = [
+    "Give Peace A Chance",
+    "Cold Turkey",
+    "Instant Karma!",
+    "Mother", "Hold On", "I Found Out", "Working Class Hero", "Isolation", "Remember", "Love", "Well Well Well", "Look At Me", "My Mummy's Dead",
+    "Power To The People",
+    "Imagine", "Crippled Inside", "Jealous Guy", "It's So Hard", "I Don't Wanna Be a Soldier", "Gimme Some Truth", "Oh My Love", "How Do You Sleep?", "How?", "Oh Yoko!",
+    "Happy Xmas (War Is Over)",
+    "Woman Is The N***** of the World", "Attica State", "New York City", "Sunday Bloody Sunday", "The Luck of the Irish", "John Sinclair", "Angela",
+    //"Sisters o Sisters", "Born in a Prison", "We're All Water",
+    "Mind Games", "Tight A$", "Aisumasen (I'm Sorry)", "One Day (At a Time)", "Bring On the Lucie (Freda Peeple)", "Intuition", "Out the Blue", "Only People", "I Know (I Know)", "You Are Here", "Meat City",
+    //"Nutopian International Anthem",
+    "Going Down on Love", "Whatever Gets You thru the Night", "Old Dirt Road", "What You Got", "Bless You", "Scared", "#9 Dream", "Surprise Surprise (Sweet Bird of Paradox)", "Steel and Glass", "Beef Jerky", "Nobody Loves You (When You're Down and Out)", "Ya Ya (Walls and Bridges album version)",
+    "Be-Bop-a-Lula", "Stand By Me", "Rip It Up/Ready Teddy", "You Can't Catch Me", "Ain't That A Shame", "Do You Want To Dance", "Sweet Little Sixteen", "Slippin' and Slidin'", "Peggy Sue", "Bring It On Home/Send Me Some Lovin'", "Bony Moronie", "Ya Ya (Rock n Roll album version)", "Just Because",
+    "(Just Like) Starting Over", "Cleanup Time", "I'm Losing You", "Beautiful Boy (Darling Boy)", "Watching the Wheels", "Woman", "Dear Yoko",
+    //"Kiss Kiss Kiss", "Give Me Something", "I'm Moving On", "Yes, I'm Your Angel", "Beautiful Boys", "Every Man Has a Woman Who Loves Him", "Hard Times Are Over"
+    "I'm Stepping Out", "I Don't Wanna Face It", "Nobody Told Me", "Borrowed Time", "(Forgive Me) My Little Flower Princess", "Grow Old with Me"
+    //"Sleepless Night", "Don't Be Scared", "O' Sanity", "Your Hands", 	"Let Me Count the Ways", "You're the One"
+
+];
+
+
+
+
+window.paul_list = [
+    "The Lovely Linda", "That Would Be Something", "Valentine Day", "Every Night", "Hot as Sun / Glasses", "Junk", "Man We Was Lonely", "Oo You", "Momma Miss America", "Teddy Boy", "Singalong Junk", "Maybe I'm Amazed", "Kreen-Akrore",
+    "Another Day", "Oh Woman, Oh Why",
+    "Too Many People", "3 Legs", "Ram On", "Dear Boy", "Uncle Albert/Admiral Halsey", "Smile Away", "Heart of the Country", "Monkberry Moon Delight", "Eat at Home", "Long Haired Lady", "Ram On (Reprise)", "The Back Seat of My Car",
+    "Mumbo", "Bip Bop", "Love Is Strange", "Wild Life", "Some People Never Know", "I Am Your Singer", "Bip Bop Link", "Tomorrow", "Dear Friend", "Mumbo Link",
+    "Give Ireland Back To The Irish",
+    "Mary Had A Little Lamb", "Little Woman Love",
+    "Hi Hi Hi", "C Moon",
+    "Big Barn Bed", "My Love", "Get on the Right Thing", "One More Kiss", "Little Lamb Dragonfly", "Single Pigeon", "When the Night", "Loup (1st Indian on the Moon)", "Medley: Hold Me Tight/Lazy Dynamite/Hands of Love/Power Cut",
+    "Live and Let Die", "I Lie Around",
+    "Helen Wheels", "Country Dreamer",
+    "Band on the Run", "Jet", "Bluebird", "Mrs. Vanderbilt", "Let Me Roll It", "Mamunia", "No Words", "Picasso's Last Words (Drink to Me)", "Nineteen Hundred and Eighty Five",
+    "Junior's Farm", "Sally G",
+    "Venus and Mars", "Rock Show", "Love in Song", "You Gave Me the Answer", "Magneto and Titanium Man", "Venus and Mars (Reprise)", "Spirits of Ancient Egypt", "Medicine Jar", "Call Me Back Again", "Listen to What the Man Said", "Treat Her Gently – Lonely Old People", "Crossroads",
+    "Let 'Em In", "The Note You Never Wrote", "She's My Baby", "Beware My Love", "Wino Junko", "Silly Love Songs", "Cook of the House", "Time to Hide", "Must Do Something About It", "San Ferry Anne", "Warm And Beautiful",
+    "Mull of Kintyre", "Girls School",
+    "London Town", "Cafe on the Left Bank", "I'm Carrying", "Backwards Traveller", "Cuff Link", "Children, Children", "Girlfriend", "I've Had Enough", "With a Little Luck", "Famous Groupies", "Deliver Your Children", "Name And Address", "Don't Let It Bring You Down", "Morse Moose And The Grey Goose",
+    "Goodnight Tonight", "Daytime Nightime Suffering",
+    "Reception", "Getting Closer", "We're Open Tonight", "Spin It On", "Again and Again and Again", "Old Siam, Sir", "Arrow Through Me", "Rockestra Theme", "To You", "After the Ball/Million Miles", "Winter Rose/Love Awake", "The Broadcast", "So Glad to See You Here", "Baby's Request",
+    "Wonderful Christmastime",
+    "Coming Up", "Temporary Secretary", "On The Way", "Waterfalls", "Nobody Knows", "Front Parlour", "Summer's Day Song", "Frozen Jap", "Bogey Music", "Darkroom", "One of These Days",
+    "Tug of War", "Take It Away", "Somebody Who Cares", "What's That You're Doing?", "Here Today", "Ballroom Dancing", "The Pound Is Sinking", "Wanderlust", "Get It", "Be What You See (Link)", "Dress Me Up As a Robber", "Ebony and Ivory",
+    "Pipes of Peace", "Say Say Say", "The Other Me", "Keep Under Cover", "So Bad", "The Man", "Sweetest Little Show", "Average Person", "Hey Hey", "Tug of Peace", "Through Our Love",
+    "We All Stand Together",
+    "No More Lonely Nights", "Not Such A Bad Boy", "No Values",
+    "Spies Like Us",
+    "Stranglehold", "Good Times Coming/Feel The Sun", "Talk More Talk", "Footprints", "Only Love Remains", "Press", "Pretty Little Head", "Move Over Busker", "Angry", "However Absurd",
+    "Once Upon A Long Ago",
+    "Kansas City", "Twenty Flight Rock", "Lawdy, Miss Clawdy", 	"Bring It On Home to Me", "Lucille", "Don't Get Around Much Anymore", "That's All Right Mama", "Ain't That a Shame", "Crackin' Up",
+    "My Brave Face", "Rough Ride", "You Want Her Too", "Distractions", "We Got Married", "Put It There", "Figure of Eight", "This One", "Don't Be Careless Love", "That Day Is Done", "How Many People", "Motor of Love", "Just Because", "Midnight Special",
+    "Off the Ground", "Looking for Changes", "Hope of Deliverance", "Mistress and Maid", "I Owe It All To You", "Biker Like an Icon", "Peace in the Neighborhood", "Golden Earth Girl", "The Lovers That Never Were", "Get Out of My Way", "Winedark Open Sea", "C'mon People",
+    "The Song We Were Singing", "The World Tonight", "If You Wanna", "Somedays", "Young Boy", "Calico Skies", "Flaming Pie", "Heaven On A Sunday", "Used To Be Bad", "Souvenir", "Little Willow", "Really Love You", "Beautiful Night", "Great Day",
+    "Bluejean Bop", "She Said Yeah", "All Shook Up", "Run Devil Run", "No Other Baby", "Lonesome Town", "Try Not to Cry", "Movie Magg", "Brown Eyed Handsome Man", "What It Is", "Coquette", "I Got Stung","Honey Hush", "Shake a Hand", "Party",
+    "Lonely Road", "From A Lover To A Friend", "She's Given Up Talking", "Driving Rain", "I Do", "Tiny Bubble", "Magic", "Your Way", "Spinning On An Axis", "About You", "Heather", "Back In The Sunshine Again", "Your Loving Flame", "Riding Into Jaipur", "Rinse the Raindrops", "Freedom",
+    "Fine Line", "How Kind of You", "Jenny Wren", "At The Mercy", "Friends To Go", "English Tea", "Too Much Rain", "A Certain Softness", "Riding To Vanity Fair", "Follow Me", "Promise To You Girl", "This Never Happened Before", "Anyway",
+    "Dance Tonight", "Ever Present Past", "See Your Sunshine", "Only Mama Knows", "You Tell Me", "Mr. Bellamy", "Gratitude", "Vintage Clothes", "That Was Me", "Feet in the Clouds", "House Of Wax", "The End Of The End", "Nod Your Head",
+    "I'm Gonna Sit Right Down and Write Myself a Letter", "Home (When Shadows Fall)", "It's Only a Paper Moon", "More I Cannot Wish You", "The Glory Of Love", "We Three (My Echo, My Shadow and Me)", "Ac-Cent-Tchu-Ate The Positive", "My Valentine", "Always", "My Very Good Friend the Milkman", "Bye Bye Blackbird", "Get Yourself Another Fool", "The Inch Worm", "Only Our Hearts",
+    "Save Us", "Alligator", "On My Way To Work", "Queenie Eye", "Early Days", "New", "Appreciate", "Everybody Out There", "Hosanna", "I Can Bet", "Looking at Her", "Road", "Scared",
+    //"Opening Station",
+    "I Don't Know", "Come On To Me", "Happy With You", "Who Cares", "Fuh You", "Confidante", "People Want Peace", "Hand In Hand", "Dominoes", "Back In Brazil", "Do It Now", "Caesar Rock", "Despite Repeated Warnings",
+    //"Station II",
+    "Hunt You Down/Naked/C-Link",
+    "Get Enough", "Home Tonight", "In A Hurry",
+    "Long Tailed Winter Bird", "Find My Way", "Pretty Boys", "Women and Wives", "Lavatory Lil", "Deep Deep Feeling", "Slidin'", "The Kiss of Venus", "Seize The Day", "Deep Down", "Winter Bird/When Winter Comes"
+];
